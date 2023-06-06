65baa97234f8942ce692e29109f19572ae128d4c
vv-248430449 <68332516+vv-248430449@users.noreply.github.com>
2023-06-06 18:19:00 +0800

    Update sidebar.tsx

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -119,9 +119,9 @@ export function SideBar(props: { className?: string }) {
       }`}
     >
       <div className={styles["sidebar-header"]}>
-        <div className={styles["sidebar-title"]}>ChatGPT Next</div>
+        <div className={styles["sidebar-title"]}>Esay Chat</div>
         <div className={styles["sidebar-sub-title"]}>
-          Build your own AI assistant.
+          Enjoying chat with me!
         </div>
         <div className={styles["sidebar-logo"] + " no-dark"}>
           <ChatGptIcon />
