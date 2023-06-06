d68adf8ae05fac7f6af48bb3dd86dbb44122a66f
vv-248430449 <68332516+vv-248430449@users.noreply.github.com>
2023-06-06 18:42:13 +0800

    Update sidebar.tsx

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -121,7 +121,7 @@ export function SideBar(props: { className?: string }) {
       <div className={styles["sidebar-header"]}>
         <div className={styles["sidebar-title"]}>Enjoying chat with me!</div>
         <div className={styles["sidebar-sub-title"]}>
-          Note: For entertainment and learning purposes only
+          Note: For entertainment and learning purposes only!
         </div>
         <div className={styles["sidebar-logo"] + " no-dark"}>
           <ChatGptIcon />
